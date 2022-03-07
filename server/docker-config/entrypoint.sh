fb4ef01502ba4c4d84a57ebd29c683f4d6bf10e6
omallassi <olivier.mallassi@gmail.com>
2022-03-07 17:52:09 +0100

    first version of DockerFile

--- server/docker-config/entrypoint.sh
+++ server/docker-config/entrypoint.sh
@@ -5,5 +5,8 @@ echo "Hey there"
 envsubst < "./config/local-template.toml" > "./config/local.toml" 
 cat ./config/local.toml
 
-export API_CONFIG_FILE=./config/local.toml
+
+# need to git your repo...
+
+export API_SERVER_CONFIG_FILE=./config/local.toml
 RUST_LOG="info,apis_catalog_server=debug" ./target/debug/apis_catalog_server
