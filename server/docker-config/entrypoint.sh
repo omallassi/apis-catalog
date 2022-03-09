78ddbb36ec2da720538b90da0ee2d9d169aedd19
omallassi <olivier.mallassi@gmail.com>
2022-03-09 18:49:15 +0100

    Docker image
    provide a way to have apis-catalog sever as a docker image

--- server/docker-config/entrypoint.sh
+++ server/docker-config/entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-echo "Hey there"
+# Create the config file w/ env var
+echo -e "\033[1;32m*** Creating config file [./config/local.toml] ***\033[0m"
 
 envsubst < "./config/local-template.toml" > "./config/local.toml" 
 cat ./config/local.toml
 
 
-# need to git your repo...
+echo -e "\033[1;32m*** Cloning the api repo [$REPO_TO_CLONE] in [$CATALOG_PATH] ***\033[0m"
+git clone $REPO_TO_CLONE $CATALOG_PATH
 
+
+# Start the server
+echo -e "\033[1;32m*** Starting apis-catalog Server ***\033[0m"
 export API_SERVER_CONFIG_FILE=./config/local.toml
 RUST_LOG="info,apis_catalog_server=debug" ./target/debug/apis_catalog_server
