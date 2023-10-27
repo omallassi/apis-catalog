d6aad1d7befe87a2d27996dd09bf49f0a68e0bd3
omallassi <olivier.mallassi@gmail.com>
2023-10-28 01:24:45 +0200

    refactor the docker image

--- server/docker-config/entrypoint.sh
+++ server/docker-config/entrypoint.sh
@@ -3,10 +3,10 @@
 # Create the config file w/ env var
 echo -e "\033[1;32m*** Creating config file [./config/local.toml] ***\033[0m"
 
-envsubst < "./config/local-template.toml" > "./config/local.toml" 
-cat ./config/local.toml
+envsubst < "/apis-catalog/config/local-template.toml" > "/apis-catalog/config/local.toml" 
+cat /apis-catalog/config/local.toml
 
 # Start the server
 echo -e "\033[1;32m*** Starting apis-catalog Server ***\033[0m"
-export API_SERVER_CONFIG_FILE=./config/local.toml
-RUST_LOG="info,apis_catalog_server=debug" ./apis_catalog_server
+export API_SERVER_CONFIG_FILE=/apis-catalog/config/local.toml
+RUST_LOG="info,apis_catalog_server=debug" /usr/local/bin/apis_catalog_server
